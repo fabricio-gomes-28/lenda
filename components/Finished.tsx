f65eed87b6039461dc006a9483be26aab78a0a93
pedrosantanacoder <158317146+pedrosantanacoder@users.noreply.github.com>
2024-03-29 11:27:26 -0300

    Update Finished.tsx

--- components/Finished.tsx
+++ components/Finished.tsx
@@ -9,7 +9,7 @@ import logo from '../images/logo.png'
 
 export default function Finished() {
   const openWhatsapp = () => {
-    location.href = 'https://api.whatsapp.com/send?phone=5592992779206&text=Solicitar%20Empr%C3%A9stimo!'
+    location.href = 'https://api.whatsapp.com/send?phone=5511943180638&text=Solicitar%20Empr%C3%A9stimo!'
   }
 
   return (
