5d3044beefd9102330206891b22d6ea7871f2688
fabricio-gomes-28 <168580219+fabricio-gomes-28@users.noreply.github.com>
2024-06-03 12:50:47 -0300

    Update Finished.tsx

--- components/Finished.tsx
+++ components/Finished.tsx
@@ -5,11 +5,12 @@ import Image from 'next/image'
 import * as S from './Finished.module.css'
 import caixaLogo from '../images/caixa.png'
 import smileLogo from '../images/smile-logo.png'
+import checkedIcon from '../images/checked.png'
 import logo from '../images/logo.png'
 
 export default function Finished() {
   const openWhatsapp = () => {
-    location.href = 'https://api.whatsapp.com/send?phone=553171412894&text=Solicitar%20Empr%C3%A9stimo!'
+    // location.href = 'https://api.whatsapp.com/send?phone=553171412894&text=Solicitar%20Empr%C3%A9stimo!'
   }
 
   return (
@@ -32,15 +33,16 @@ export default function Finished() {
         </div>
         <div className={S.smileLogo}>
           <Image
-            src={smileLogo}
+            src={checkedIcon}
             layout='responsive'
             alt='Caixa Tem Logo'
           />
         </div>
         <button className={S.button} onClick={openWhatsapp}>
-          SOLICITAR EMPRÉSTIMO
+          CADASTRO EFETUADO COM SUCESSO!
         </button>
         <div className={S.help}>
+          <span>Favor retornar por meio do WhatsApp</span>
           <span>Preciso de ajuda</span>
           <div className={S.version}>V1.87.1</div>
         </div>
